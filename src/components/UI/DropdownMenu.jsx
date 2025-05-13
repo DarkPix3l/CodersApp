a8600de97f577953f4190025875a74cd8bca3b9f
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-05-13 22:32:29 +0200

    drop down menu style update

--- src/components/UI/DropdownMenu.jsx
+++ src/components/UI/DropdownMenu.jsx
@@ -3,7 +3,7 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 export default function DropdownMenu() {
   return (
     <Menu>
-      <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-700 data-open:bg-gray-700">
+      <MenuButton className="hidden lg:inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-700 data-open:bg-gray-700">
         My account
       </MenuButton>
       <MenuItems
