0d789f199192fa76fa017ee6fa95e471916bd798
Greta M. <181118522+DarkPix3l@users.noreply.github.com>
2025-10-01 21:29:47 +0200

    Delete other untracked files

--- managers_portal/src/app/_components/LoginImg.jsx
+++
@@ -1,9 +0,0 @@
-function LoginImg() {
-  return (
-    <div className="bg-[#23155b] flex items-center justify-center sm:h-[50vh] md:h-[100vh] sm:w-full md:w-[50%]">
-      <img src="./img/coding.png" alt="Image of an avid programmer" />
-    </div>
-  );
-}
-
-export default LoginImg
--- managers_portal/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx"
-
-/**
- * Utility function to combine class names.
- * Wraps clsx for easier use.
- */
-export function cn(...inputs) {
-  return clsx(...inputs)
-}
